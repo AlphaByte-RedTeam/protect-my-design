2d653b2b31cf1333e4f3844a1a5eed1f040303d9
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-12-30 16:22:07 +0700

    style: remove invert class on dark mode

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,15 +6,7 @@ type LogoProps = {
 };
 
 const Logo = ({ width = 128, height = 128 }: LogoProps) => {
-  return (
-    <Image
-      className="dark:invert"
-      src="/logo.png"
-      alt="Logo "
-      width={width}
-      height={height}
-    />
-  );
+  return <Image src="/logo.png" alt="Logo " width={width} height={height} />;
 };
 
 export default Logo;
