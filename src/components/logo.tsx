abfb9c42421bf7be51364e31beeddc2cf2fda89d
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-12-31 09:27:40 +0700

    style: change default value width and height

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,16 +6,19 @@ type LogoProps = {
   height?: number;
 };
 
-const Logo = ({ width = 128, height = 128 }: LogoProps) => {
+const Logo = ({ width = 32, height = 32 }: LogoProps) => {
   return (
-    <Link href="/" className="flex flex-row justify-center items-center">
+    <Link
+      href="/"
+      className="flex gap-1 flex-row justify-center items-center h-full"
+    >
       <Image
         src="/logo.png"
         alt="Logo ProtectMyDesign"
         width={width}
         height={height}
       />
-      <p className="text-slate-12 dark:text-white w-24 leading-5 font-semibold text-xl">
+      <p className="text-slate-12 dark:text-white w-24 leading-4 font-bold text-sm">
         ProtectMy Design
       </p>
     </Link>
