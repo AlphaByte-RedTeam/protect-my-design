0d825c0cc5e47e1072110c043856c52bf37189df
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-12-31 10:19:37 +0700

    feat: add footer and adjust layout position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import {
   DropzoneContent,
   DropzoneEmptyState,
 } from "@/components/dropzone";
+import Footer from "@/components/footer";
 import Logo from "@/components/logo";
 import ThemeToggler from "@/components/theme-toggler";
 import { useSupabaseUpload } from "@/hooks/use-supabase-upload";
@@ -12,7 +13,6 @@ import { useSupabaseUpload } from "@/hooks/use-supabase-upload";
 export default function Home() {
   const props = useSupabaseUpload({
     bucketName: "preview-assets",
-    path: "original",
     allowedMimeTypes: ["image/*"],
     maxFiles: 2,
     maxFileSize: 1000 * 1000 * 10, // 10MB,
@@ -20,7 +20,7 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-brand-1 font-sans dark:bg-dark-brand-1">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-16 px-4 bg-brand-1 dark:bg-dark-brand-1 sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-16 px-4 bg-brand-1 dark:bg-dark-brand-1 sm:items-center">
         <div className="flex flex-row justify-between w-full items-center">
           <Logo />
           <ThemeToggler />
@@ -31,6 +31,7 @@ export default function Home() {
             <DropzoneContent />
           </Dropzone>
         </div>
+        <Footer />
       </main>
     </div>
   );
